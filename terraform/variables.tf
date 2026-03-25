ba7e407477eab8ddcebffb8163f56a9101d775d7
Abhinav Mishra <83443043+mishraabhinav304@users.noreply.github.com>
2026-03-25 13:05:29 +0100

    Change default AWS region and AMI ID
    Updated AWS region and AMI ID for Ubuntu in ap-south-1.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region"       { default = "us-east-1" }
+variable "aws_region"       { default = "ap-south-1" }
 variable "instance_type"    { default = "t3.medium" }
-variable "ami_id"           { default = "ami-0c02fb55956c7d316" } # Amazon Linux 2 us-east-1
+variable "ami_id"           { default = "ami-07216ac99dc46a187" } # Ubuntu ap-south-1
 variable "public_key_path"  { default = "~/.ssh/id_rsa.pub" }
